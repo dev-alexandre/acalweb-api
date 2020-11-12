323666d9cce8765ff10f1f7b477f68ac528e9d68
Alexandre Queiroz <alexandre.queiroz@br.experian.com>
2020-11-12 16:23:22 -0300

    [acal][api]

---
+++ src/test/java/br/com/acal/acalapi/AcalapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.acal.acalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcalapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
